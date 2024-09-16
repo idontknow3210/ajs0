a9bdcc732adec6e2358750150e46940e88a45d1a
Yura <bobr515yura@yandex.ru>
2024-09-16 13:26:22 +0300

    HW2

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-export default class Validator {
-    validateUsername(name) {
-      this.name = name;
-      return (/^[a-zA-Z][a-zA-Z0-9\-_]+[a-zA-Z]$/.exec(this.name) !== null)
-      && (!/[0-9]{4}/.test(this.name));
-    }
-}
-
